8f7f58a89ace1fb324a3fc2cbbbfa79e2d271447
LeoMor1 <leo.morinjung@gmail.com>
2026-04-07 19:06:05 +0200

    feat: random function

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,9 +1,25 @@
+use rand::{Rng, RngExt};
+
 #[derive(Debug)]
 pub struct Network {
     layers: Vec<Layer>,
 }
 
+#[derive(Debug)]
+pub struct LayerToplology {
+    pub neurons: usize,
+}
+
 impl Network {
+    pub fn random(rng: &mut dyn Rng ,layers: &[LayerToplology]) -> Self {
+        assert!(layers.len() > 1);
+        let layers = layers
+            .windows(2)
+            .map(|layers| Layer::random(rng, layers[0].neurons, layers[1].neurons))
+            .collect();
+        Self { layers }
+    }
+
     pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         // layers = niveaux de reseaux donc layer 1 calculer et donne sont resultat a layer 2
         // la boucle simule ce transfers de valeurs
@@ -19,6 +35,13 @@ struct Layer {
 }
 
 impl Layer {
+    fn random(rng: &mut dyn Rng, input_size: usize, output_size: usize) -> Self {
+        let neurons = (0..output_size)
+            .map(|_| Neuron::random(rng, input_size))
+            .collect();
+        Self { neurons }
+    }
+
     fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         self.neurons
             .iter()
@@ -34,6 +57,16 @@ struct Neuron {
 }
 
 impl Neuron {
+    fn random(rng : &mut dyn Rng, input_size: usize) -> Self{
+        let bias = rng.random_range(-1.0..=1.0);
+
+        let weights = (0..input_size)
+            .map(|_| rng.random_range(-1.0..=1.0))
+            .collect();
+
+        Self { bias, weights }
+    }
+
     fn propagate(&self, inputs: &[f32]) -> f32 {
         assert_eq!(inputs.len(), self.weights.len());
         let output = inputs
