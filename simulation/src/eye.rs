e21243ca96f486015640e7cd60da7d96bc33a515
LeoMor1 <leo.morinjung@gmail.com>
2026-06-17 18:32:57 +0200

    feat: eye

--- simulation/src/eye.rs
+++ simulation/src/eye.rs
@@ -38,12 +38,136 @@ impl Eye {
         rotation: nalgebra::Rotation2<f32>,
         foods: &[Food],
     ) -> Vec<f32> {
-        todo!()
+        let mut cells = vec![0.0; self.cells];
+        for food in foods {
+            let vec = food.position - position;
+
+            let dist = vec.norm();
+            if dist >= self.fov_range {
+                continue;
+            }
+
+            let angle =
+                nalgebra::Rotation2::rotation_between(&nalgebra::Vector2::y(), &vec).angle();
+
+            let angle = angle - rotation.angle();
+
+            let angle = nalgebra::wrap(angle, -PI, PI);
+
+            if angle < -self.fov_angle / 2.0 || angle > self.fov_angle / 2.0 {
+                continue;
+            }
+
+            let angle = angle + self.fov_angle / 2.0;
+
+            let cell = angle / self.fov_angle;
+
+            let cell = cell * (self.cells as f32);
+
+            let cell = (cell as usize).min(cells.len() - 1);
+
+            let energy = (self.fov_range - dist) / self.fov_range;
+
+            cells[cell] += energy;
+        }
+
+        cells
     }
 }
 
 impl Default for Eye {
     fn default() -> Self {
         Self::new(FOV_RANGE, FOV_ANGLE, CELLS)
+    }
+}
+
+mod tests {
+    use std::f32::consts::FRAC_PI_2;
+
+    use crate::food;
+
+    use super::*;
+    use test_case::test_case;
+
+    const TEST_EYE_CELLS: usize = 13;
+
+    struct TestCase {
+        foods: Vec<Food>,
+        fov_range: f32,
+        fov_angle: f32,
+        x: f32,
+        y: f32,
+        rot: f32,
+        expected_vision: &'static str,
+    }
+
+    impl TestCase {
+        fn run(self) {
+            let eye = Eye::new(self.fov_range, self.fov_angle, TEST_EYE_CELLS);
+
+            let actual_vision = eye.process_vision(
+                nalgebra::Point2::new(self.x, self.y),
+                nalgebra::Rotation2::new(self.rot),
+                &self.foods,
+            );
+            let actual_vision: Vec<&str> = actual_vision
+                .into_iter()
+                .map(|cell| {
+                    if cell >= 0.7 {
+                        "#"
+                    } else if cell >= 0.3 {
+                        "+"
+                    } else if cell > 0.0 {
+                        "."
+                    } else {
+                        " "
+                    }
+                })
+                .collect();
+            let actual_vision = actual_vision.join("");
+            assert_eq!(actual_vision, self.expected_vision);
+        }
+    }
+
+    fn food(x: f32, y: f32) -> Food {
+        Food {
+            position: nalgebra::Point2::new(x, y),
+        }
+    }
+
+    #[test_case(1.0, "      +      ")]
+    #[test_case(0.9, "      +      ")]
+    #[test_case(0.8, "      +      ")]
+    #[test_case(0.7, "      .      ")]
+    #[test_case(0.6, "      .      ")]
+    #[test_case(0.5, "             ")]
+    #[test_case(0.4, "             ")]
+    #[test_case(0.3, "             ")]
+    #[test_case(0.2, "             ")]
+    #[test_case(0.1, "             ")]
+    fn fov_range(fov_range: f32, expected_vision: &'static str) {
+        TestCase {
+            foods: vec![food(0.5, 1.0)],
+            fov_angle: FRAC_PI_2,
+            x: 0.5,
+            y: 0.5,
+            rot: 0.0,
+            expected_vision,
+            fov_range,
+        }
+        .run()
+    }
+
+    fn rotation(rot: f32, expected_vision: &'static str) {
+        TestCase {
+            foods: vec![food(0.5, 1.0)],
+            fov_angle: FRAC_PI_2,
+            fov_range: 2.0 * PI,
+            x: 0.5,
+            y: 0.5,
+            rot,
+            expected_vision,
+        }
+        .run();
     }
 }
